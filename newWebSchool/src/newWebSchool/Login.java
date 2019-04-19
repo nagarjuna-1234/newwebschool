f2fa0ce3e20060441a5f5b7b4a84aeeddea32200
venky <venky@venkatesh>
2019-04-19 09:31:10 +0530

    second commit

--- newWebSchool/src/newWebSchool/Login.java
+++ newWebSchool/src/newWebSchool/Login.java
@@ -5,6 +5,8 @@ public class Login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("nagarjuna is .................");
+//second commit
+System.out.println("vinay is ..............................");
 	}
 
 }
