e82e914dd97fae8092fc88c929d10e5f447bfea9
venky <venky@venkatesh>
2019-04-19 09:24:32 +0530

    this is my first commit

---
+++ newWebSchool/src/newWebSchool/Login.java
@@ -0,0 +1,10 @@
+package newWebSchool;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("nagarjuna is .................");
+	}
+
+}
